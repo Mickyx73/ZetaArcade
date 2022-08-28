158158c67bf07511e95fee4740e60fd16cd17888
Shino <shachcha01@gmail.com>
2022-08-29 01:12:04 +0600

    swiper sliders added

--- js/main.js
+++ js/main.js
@@ -17,3 +17,25 @@ document.querySelector('#bell-icon').onclick = () =>{
 }
 
 //swiper
+var swiper = new Swiper(".trending-content", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 15,
+      },
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 20,
+      },
+    },
+  });
