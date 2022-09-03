8bfbb493e7e43bd342102ef2862c33c3b19530f5
Shino <shachcha01@gmail.com>
2022-09-03 12:53:27 +0600

    log

--- php/loginpop.php
+++ php/loginpop.php
@@ -1,15 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/loginpop.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.1.2/css/fontawesome.min.css">
-    
+
     <title>Login</title>
 </head>
+
 <body>
-    
+    <div class="container">
+        <p class="login-text"> Login Using Socials</p>
+        <div class="login-socials">
+            <a href="#"><i class="fa-brands fa-steam"></i>Steam</a>
+            <a href="#"><i class="fa-brands fa-playstation"></i>Playstation</a>
+            <a href="#"><i class="fa-brands fa-xbox"></i>Xbox</a>
+            <a href="#"><i class="fa-brands fa-google"></i>Google</a>
+        </div>
+        <form class="login-email">
+            <p class="login-text">Login Using Email</p>
+            <div class="input-group">
+                <input type="email" placeholder="Email" required>
+            </div>
+            <div class="input-group">
+                <input type="password" placeholder="Password" required>
+            </div>
+            <div class="input-group">
+                <button class="btn"></button>
+            </div>
+        </form>
+    </div>
+
 </body>
+
 </html>
